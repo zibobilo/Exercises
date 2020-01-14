c8587be226e646aeaa29c7e2c05b9ebc7e6749c1
Raphael Burkhardt <burkhardtraphael@gmail.com>
2020-01-14 15:21:14 -0500

    Update and rename exercise 33.js to exercise 033.js

--- exercise 033.js
+++ exercise 033.js
@@ -13,15 +13,12 @@ for(den = 10 ; den < 100 ; den++){
     for (num = 10 ; num <= den ; num++){
         if(den!==num){
             if(String(den)[0] == String(num)[1] || String(den)[1] == String(num)[0]){
-
                 if(Number(String(num)[0])/Number(String(den)[1]) == num/den){
-
                     nonTrivial.push(`${num}/${den} = ${Number(String(num)[0])}/${Number(String(den)[1])}`)
                     prodDen *= den
                     prodNum *= num
 
                 } else if(Number(String(num)[1])/String(den)[0] == num/den){
-
                     nonTrivial.push(`${num}/${den} = ${Number(String(num)[1])}/${Number(String(den)[0])}`)
                     prodDen *= den
                     prodNum *= num
@@ -31,5 +28,6 @@ for(den = 10 ; den < 100 ; den++){
         }         
     }
 }
+
 console.log(nonTrivial)
 console.log(prodNum/prodDen)
