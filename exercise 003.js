9c70aa350ddf33bf4534b8b34002cc264cbf846f
Raphael Burkhardt <burkhardtraphael@gmail.com>
2020-01-14 15:20:46 -0500

    Update and rename exercise 3.js to exercise 003.js

--- exercise 003.js
+++ exercise 003.js
@@ -4,12 +4,11 @@
 // The prime factors of 13195 are 5, 7, 13 and 29.
 // What is the largest prime factor of the number 600851475143 ?
 
-
 let exercise = 600851475143;
 for (i=2; i<10000; i++){
     if (exercise % i == 0){
         exercise = exercise/i;
         console.log(`Prime factor: ${i}`);
         console.log(`${exercise}`);
-    } else{}
+    }
 }
