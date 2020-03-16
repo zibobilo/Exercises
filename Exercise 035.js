ad6aa59bf63850c81e3e495b859e3bdc5bb5d620
Raphael Burkhardt <burkhardtraphael@gmail.com>
2020-03-16 19:57:36 -0400

    exercise done but slow(to be fixed)

---
+++ Exercise 035.js
@@ -0,0 +1,58 @@
+// Exercise 35
+// https://projecteuler.net/problem=35
+
+// The number, 197, is called a circular prime because all rotations of the digits: 197, 971, and 719, are themselves prime.
+// There are thirteen such primes below 100: 2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, and 97.
+// How many circular primes are there below one million?
+
+
+let limit = 1000000;
+let answer = 0;
+let results = [];
+let primes = [];
+
+function isPrime(number) {
+    if (number == 0 || number == 1) {
+        return false
+    }
+    if (number == 2) {
+        return true
+    }
+
+    if (number % 2 == 0) {
+        return false
+    }
+
+    for (let i = 3; i < number; i += 2) {
+        if (number % i != 0) { }
+        else { return false }
+    }
+    return true
+}
+
+for (let i = 0; i < limit; i++) {
+    let rotationTesting = []
+    let toAddAtTheBack
+
+    if (isPrime(i)) {
+        for (let s = 0; s < String(i).length; s++) {
+            rotationTesting.push(String(i)[s])
+        }
+        let testCount = 0
+        for (let r = 0; r < String(i).length - 1; r++) {
+            toAddAtTheBack = rotationTesting.pop(Number(rotationTesting.length));
+            rotationTesting.unshift(toAddAtTheBack);
+            if (isPrime(Number(rotationTesting.join("")))) {
+                testCount++
+            } else { testCount = 0; break; }
+        }
+        if (testCount == Number(String(i).length) - 1 || Number(String(i).length) == 1) {
+            answer ++
+            testCount = 0
+        }
+    }
+}
+
+// needs improvement on execution time ~66s
+console.log(answer)
+
