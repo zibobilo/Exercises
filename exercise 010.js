89723dd5a1d93388d02ca1cd80ffca04307b30b3
Zibobilo <burkhardtraphael@gmail.com>
2020-12-29 19:44:46 -0500

    revision algo

--- exercise 010.js
+++ exercise 010.js
@@ -3,54 +3,19 @@
 // The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 // Find the sum of all the primes below two million.
 
-
-let primes = [];
-let count = 1;
-let stopProcess = 1000000;
-let sum = 0;
-
-for (let i = 2; i < stopProcess; i++) {
-    primes[i] = true;
+let sum = 2
+// Itarate through each number from 3 to below 2 million
+for (let i = 3; i < 2000000; i += 2) {
+    if (isPrime(i)) sum += i
 }
 
-let limit = Math.sqrt(stopProcess);
-
-for (let i = 2; i < limit; i++) {
-    for (let j = i * i; j < stopProcess; j += i) {
-        primes[j] = false;
-    }
-}
-
-for (let i = 2; i < stopProcess; i++) {
-    if (primes[i] === true) {
-        count += 1
-        sum += i
-    }
+function isPrime (num) {
+  for (let j = 3; j <= Math.floor(Math.sqrt(num)); j+=2) {
+    if (!(num % j)) return false
+  }
+  return true
 }
 console.log(sum)
 
-
-     // let primes = [2n]
-// function getNextPrime() {
-        //     for (let i = lastPrime + 1n; i <= n; i++) {
-        //         for (let j = 0; j < primes.length; j++) {
-        //             if (i % primes[j] === 0n) break
-        //             if (j === primes.length - 1) {
-        //                 primes.push(i)
-        //                 lastPrime = i
-        //                 return
-        //             }
-        //         }
-        //     }
-        // }
-        // let lastPrime = primes[primes.length-1]
-        // while(lastPrime < n) {
-            
-        //     if (n % lastPrime === 0n) {
-        //         n = n/lastPrime
-        //     }
-        //     getNextPrime()
-        // }
-        // console.log(String(lastPrime).slice(0, lastPrime.length))
-    // }
-    
+// => 142913828922
+// => +-0.5 s
